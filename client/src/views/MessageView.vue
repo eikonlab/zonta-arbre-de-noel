0bb9dc9c35e2bf5f3e1d830d467c516d508da87f
jminguely <jminguely@gmail.com>
2025-11-19 16:20:08 +0100

    Disable siple mode

--- client/src/views/MessageView.vue
+++ client/src/views/MessageView.vue
@@ -33,7 +33,7 @@ const API_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
 const socket = io(API_URL, { transports: ["websocket"] });
 
 // DEBUG MODE: Set to true for simple display without path animation (Raspberry Pi)
-const SIMPLE_MODE = true;
+const SIMPLE_MODE = false;
 
 // Messages/state
 const messages = ref([]);
